787ae7a9baec8b2621fca5f3b2ef2a0fa682d48d
devu5-2 <153796842+devu5-2@users.noreply.github.com>
2023-12-20 12:17:35 +0000

    container config done

--- variable.js
+++ variable.js
@@ -1,1 +1,12 @@
 console.log("hello world")
+const accountId=11576
+let accountEmail="dev@google.com"
+var accountPassword="1234"
+accoountCity="jaipur"
+let accountState;
+
+accountEmail="hc@hc.com"
+accountPassword="76543"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
