85a658eb7e6704613c54036570c97c54d4681ecb
devu5-2 <153796842+devu5-2@users.noreply.github.com>
2023-12-20 12:57:09 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,13 @@
+let name="dev"
+let age="18"
+let isloggedin=false
+let state;
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+console.log(typeof undefined);
+console.log(typeof null);
