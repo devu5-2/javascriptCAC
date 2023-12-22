b86bcf714b15ea4a73036eca4253889382e46d30
devu5-2 <153796842+devu5-2@users.noreply.github.com>
2023-12-22 06:22:50 +0000

    string

---
+++ strings.js
@@ -0,0 +1,28 @@
+const name="dev"
+const repocount=50
+// console.log(name+repocount+"value");
+console.log(`hello my name is ${name} and my exxperience is of ${repocount}`)
+
+const gameName= new String('dev-dot,com')
+// console.log(gameName[5]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(8));
+// console.log(gameName.indexOf('t'));
+// const newString = gameName.substring(5, 8)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
